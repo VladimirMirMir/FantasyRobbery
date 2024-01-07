1d3a6cde486aa830843bf61bb08838cef6da9ae8
VladimirMirMir <miroschnikovvladimir@yandex.ru>
2024-01-07 18:03:17 +0300

    Lobby wip

--- Assets/FantasyRobbery/Scripts/Ui/Screens/CreateLobbyScreen.cs
+++ Assets/FantasyRobbery/Scripts/Ui/Screens/CreateLobbyScreen.cs
@@ -10,12 +10,18 @@ namespace FantasyRobbery.Scripts.Ui
         [SerializeField] private TMP_Dropdown _lobbyTypeDropdown;
         [SerializeField] private Slider _maxPlayersCountSlider;
         [SerializeField] private Button _createLobbyBtn;
+        [SerializeField] private Button _backBtn;
 
         protected override void OnShowComplete()
         {
             _createLobbyBtn.onClick.AddListener(() =>
             {
                 MultiplayerService.CreateLobby((ELobbyType)_lobbyTypeDropdown.value, (int)_maxPlayersCountSlider.value);
+            });
+            _backBtn.onClick.AddListener(() =>
+            {
+                UiService.Hide<CreateLobbyScreen>();
+                UiService.Show<MainMenuScreen>();
             });
         }
     }
